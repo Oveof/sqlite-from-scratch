57589c3be205d5fd683d5f73584b5b4eb1d58ed4
oveof <ovedy@oftedal.com>
2024-09-17 19:49:32 +0000

    .

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,34 @@
 const std = @import("std");
 
+const inputbuffer = struct { buffer: *c_char, buffer_length: usize, input_length: usize };
+
+fn read_input() ![]const u8 {
+    const stdin = std.io.getStdIn().reader();
+    const bare_line = try stdin.readUntilDelimiterAlloc(
+        std.heap.page_allocator,
+        '\n',
+        8192,
+    );
+    const line = std.mem.trim(u8, bare_line, "\n");
+    return line;
+}
+
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    // const input_buffer = inputbuffer{ .buffer = null, .input_length = 0, .buffer_length = 0 };
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    while (true) {
+        const line = try read_input();
+        defer std.heap.page_allocator.free(line);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+        if (std.mem.eql(u8, line[0..1], ".")) {
+            std.debug.print("{s}", .{line[0..1]});
+        }
+    }
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+// test "simple test" {
+//     var list = std.ArrayList(i32).init(std.testing.allocator);
+//     defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+//     try list.append(42);
+//     try std.testing.expectEqual(@as(i32, 42), list.pop());
+// }
